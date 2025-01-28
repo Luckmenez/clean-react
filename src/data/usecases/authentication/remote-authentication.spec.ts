5f519d5bd07ca14f9f38bc9f29b676cc243689a1
Lucas Menezes <luckmenez@gmail.com>
2025-01-27 23:13:26 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct URL

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,34 +1,21 @@
 import { describe, test, expect } from 'vitest';
-
-interface HttpPostClient {
-    post(url: string): Promise<void>;
-}
-
-class RemoteAuthentication {
-    constructor(
-        private readonly url: string,
-        private readonly httpPostClient: HttpPostClient,
-    ) {}
-
-    auth(): void {
-        this.httpPostClient.post(this.url);
-    }
-}
+import { HttpPostClient } from '../../protocols/http/http-post-client';
+import { RemoteAuthentication } from './remote-authentication';
 
 describe('RemoteAuthentication', () => {
-    test('Should call HttpPostClient with correct URL', () => {
-        class HttpPostClientSpy implements HttpPostClient {
-            url?: string;
+  test('Should call HttpPostClient with correct URL', () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string;
 
-            async post(url: string): Promise<void> {
-                this.url = url;
-                return Promise.resolve();
-            }
-        }
-        const url = 'any_url';
-        const httpPostClientSpy = new HttpPostClientSpy();
-        const sut = new RemoteAuthentication(url, httpPostClientSpy);
-        sut.auth();
-        expect(httpPostClientSpy.url).toBe(url);
-    });
+      async post(url: string): Promise<void> {
+        this.url = url;
+        return Promise.resolve();
+      }
+    }
+    const url = 'any_url';
+    const httpPostClientSpy = new HttpPostClientSpy();
+    const sut = new RemoteAuthentication(url, httpPostClientSpy);
+    sut.auth();
+    expect(httpPostClientSpy.url).toBe(url);
+  });
 });
